933606ae3e3b9385ebcd117db4e2d7aa200c3b5b
wfreau <wfreau@gmail.com>
2019-03-04 21:29:57 +0100

    Revert to JUnit Test

--- src/main/java/org/mines/douai/tp/spring/YellowJacketServlet.java
+++ src/main/java/org/mines/douai/tp/spring/YellowJacketServlet.java
@@ -1,14 +1,25 @@
 package org.mines.douai.tp.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet(name = "YellowJacketServlet")
-public class YellowJacketServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+@WebServlet(name = "YellowJacketServlet")
+public class YellowJacketServlet extends HttpServlet {
+    
+    @Autowired
+    private YellowJacketServiceImpl service;
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
     }
     
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
     }
 }
